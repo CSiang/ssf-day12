d6033a80bd6601bbbccc6c105de19ad423a8b9d4
unknown <you@example.comcscorpio87@gmail.com>
2023-02-07 10:10:01 +0800

    initial commit

---
+++ src/test/java/ssf2022/day12/Day12ApplicationTests.java
@@ -0,0 +1,13 @@
+package ssf2022.day12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
